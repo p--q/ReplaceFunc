490d8d1673e1d7e62220127d061ec61c8a73abd7
pq <pq.blogspot@gmail.com>
2017-05-30 13:46:34 +0900

    デコレータ

--- RepalceFunc/src/replacefunc.py
+++ RepalceFunc/src/replacefunc.py
@@ -1,30 +1,40 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
-def targetFunc():  # この関数の中の関数を置換する。
-    print("ターゲットの関数")
+
 def newFunc(arg):  # 置換後の新しい関数。
     print("置換後の関数で{}を出力。".format(arg))
     
 from functools import wraps
 import inspect   
-def replaceFunc(oldfunc, newfunc):  # targetfunc内のoldfuncをnewfuncに置換する。
+def replaceFunc(oldfunc, newfunc):  # func内のoldfuncをnewfuncに置換する。
     def decorate(func):
         module_name = inspect.getmodulename(__file__)  # newfuncのあるモジュール名を取得。
-        srclines = inspect.getsource(func).splitlines()  # tagergetfuncのソースを1行ずつ要素にしたリストを取得。
-        srclines.insert(1,"""
+        srclines = inspect.getsource(func).splitlines()  # funcのソースを1行ずつ要素にしたリストを取得。
+        for n, line in enumerate(srclines):
+            if '@replaceFunc' in line:
+                break        
+        else:
+            n = -1
+        srclines.insert(n+2,"""
     from {0} import {2}
     {1} = {2}
-        """.format(module_name, oldfunc.__name__, newfunc.__name__))  # targetfuncのソースを改変する。
-        src = '\n'.join(srclines)  # targetfuncのソースを作成。
+        """.format(module_name, oldfunc.__name__, newfunc.__name__))  # funcのソースを改変する。
+        src = '\n'.join(srclines[n+1:])  # funcのソースを作成。
         temp = dict()  # exec()の仮想モジュールの名前空間を受けとる辞書。
         exec(compile(src,'virtual_module','exec'), temp, temp)  # srcをコンパイルしてtempに取得。
         @wraps(func)
         def wrapper(*args, **kwargs):
-            return temp[func.__name__](*args, **kwargs)  # 作り替えたtargetfuncを返す。
+            return temp[func.__name__](*args, **kwargs)  # 作り替えたfuncを返す。
         return wrapper
     return decorate
 
+@replaceFunc(print, newFunc)
+def targetFunc():  # この関数の中の関数を置換する。
+    print("ターゲットの関数")
+
 if __name__ == "__main__":
-    targetFunc = replaceFunc(print, newFunc)(targetFunc)
     targetFunc()
+    
+    
+
     
