1bea1f385f1d71da6b13dfa11daad5c421684ba3
pq <pq.blogspot@gmail.com>
2017-05-30 17:19:54 +0900

    HP

--- RepalceFunc/src/replacefunc.py
+++ RepalceFunc/src/replacefunc.py
@@ -28,13 +28,16 @@ def replaceFunc(oldfunc, newfunc):  # func内のoldfuncをnewfuncに置換する
         return wrapper
     return decorate
 
-def newFunc(arg):  # 置換後の新しい関数。
-    print("置換後の関数で{}を出力。".format(arg)) 
+# def newFunc(arg):  # 置換後の新しい関数。
+#     print("置換後の関数で{}を出力。".format(arg)) 
       
 # def targetFunc():  # この関数の中の関数を置換する。デコレータ式を使わない時。
 #     print("ターゲットの関数")
 
 if __name__ == "__main__":
+    def newFunc(arg):  # 置換後の新しい関数。
+        print("置換後の関数で{}を出力。".format(arg)) 
+    
     @replaceFunc(print, newFunc)  # 引数付きデコレータ。グローバルスコープで使うと2回実行されてしまう。
     def targetFunc():  # この関数の中の関数を置換する。
         print("ターゲットの関数")
