5b375d4bcc10d88d9d3937fca44d949adb893f12
pq <pq.blogspot@gmail.com>
2017-05-30 17:04:31 +0900

    Squashed commit of the following:
    commit 3034c920d30a4d6233a5ab177e222e66c5b02a37
    Author: pq <pq.blogspot@gmail.com>
    Date:   Tue May 30 17:00:45 2017 +0900
    
    	replaceFuncを引数付きデコレータに対応。
    
    commit afaea6e1564c9550d1f632f0e81d8fd62ca44b16
    Author: pq <pq.blogspot@gmail.com>
    Date:   Tue May 30 13:42:08 2017 +0900
    
    	デコレータ
    
    commit 66f7880f3ba9e5afb815cd34476cafd19b661647
    Author: pq <pq.blogspot@gmail.com>
    Date:   Tue May 30 00:48:31 2017 +0900
    
    	引数付きデコレータ式
    
    commit 1b80148e350f7e5567527178d03c8a80eafb14aa
    Author: pq <pq.blogspot@gmail.com>
    Date:   Mon May 29 23:54:11 2017 +0900
    
    	動かない。
    
    commit b93402c513d57576206c2b8157621d7be6c47c76
    Author: pq <pq.blogspot@gmail.com>
    Date:   Mon May 29 23:41:15 2017 +0900
    
    	引数付きデコレーター。

--- RepalceFunc/src/replacefunc.py
+++ RepalceFunc/src/replacefunc.py
@@ -1,21 +1,47 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
-def targetFunc():  # この関数の中の関数を置換する。
-    print("ターゲットの関数")
+from functools import wraps
+import inspect   
+def replaceFunc(oldfunc, newfunc):  # func内のoldfuncをnewfuncに置換する。
+    def decorate(func):
+        module_name = inspect.getmodulename(__file__)  # newfuncのあるモジュール名を取得。
+        srclines = inspect.getsource(func).splitlines()  # funcのソースを1行ずつ要素にしたリストを取得。
+        for n, line in enumerate(srclines):  # デコレータ式の行番号を取得。
+            if '@replaceFunc' in line:
+                break        
+        else:  # デコレータ式がないとき
+            n = -1
+        tab = "    "  # タブの文字列をスペース4個とする。
+        defline = srclines[n+1].expandtabs(4)  # 関数定義の先頭行をタブを4つのスペースに置換して取得。
+        t = defline.replace(defline.lstrip(tab), "").count(tab)  # 行頭のtabの数を取得。        
+        srclines.insert(n+2, tab * (t + 1) + "from {0} import {2}; {1} = {2}".format(module_name, oldfunc.__name__, newfunc.__name__))  # funcのソースを改変する。
+        src = '\n'.join(srclines[n+1:])  # funcのソースを再作成。
+        ind = ""  # インデントの処理。
+        for i in range(t):
+            ind += tab * i + 'if 1:\n'
+        src = ind + src
+        temp = dict()  # exec()の仮想モジュールの名前空間を受けとる辞書。
+        exec(compile(src,'virtual_module','exec'), temp, temp)  # srcをコンパイルしてtempに取得。
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return temp[func.__name__](*args, **kwargs)  # 作り替えたfuncを返す。
+        return wrapper
+    return decorate
+
 def newFunc(arg):  # 置換後の新しい関数。
-    print("置換後の関数で{}を出力。".format(arg))
-def replaceFunc(targetfunc, oldfunc, newfunc):  # targetfunc内のoldfuncをnewfuncに置換する。
-    import inspect
-    module_name = inspect.getmodulename(__file__)  # newfuncのあるモジュール名を取得。
-    srclines = inspect.getsource(targetfunc).splitlines()  # tagergetfuncのソースを1行ずつ要素にしたリストを取得。
-    srclines.insert(1,"""
-    from {0} import {2}
-    {1} = {2}  
-    """.format(module_name, oldfunc.__name__, newfunc.__name__))  # targetfuncのソースを改変する。
-    src = '\n'.join(srclines)  # targetfuncのソースを作成。
-    temp = dict()  # exec()の仮想モジュールの名前空間を受けとる辞書。
-    exec(compile(src,'virtual_module','exec'), temp, temp)  # srcをコンパイルしてtempに取得。
-    return temp[targetfunc.__name__]  # 作り替えたtargetfuncを返す。
+    print("置換後の関数で{}を出力。".format(arg)) 
+      
+# def targetFunc():  # この関数の中の関数を置換する。デコレータ式を使わない時。
+#     print("ターゲットの関数")
+
 if __name__ == "__main__":
-    targetFunc = replaceFunc(targetFunc, print, newFunc)
+    @replaceFunc(print, newFunc)  # 引数付きデコレータ。グローバルスコープで使うと2回実行されてしまう。
+    def targetFunc():  # この関数の中の関数を置換する。
+        print("ターゲットの関数")
+    
+#     targetFunc = replaceFunc(print, newFunc)(targetFunc)  # デコレータ式を使わない方法。
+    
     targetFunc()
+    
+    
+    
