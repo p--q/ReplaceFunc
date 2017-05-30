634d6ba3ccf5bd520956b463cb6fe96524d8e58c
pq <pq.blogspot@gmail.com>
2017-05-30 23:00:06 +0900

    複数の関数置換に対応。

--- RepalceFunc/src/example.py
+++ RepalceFunc/src/example.py
@@ -3,8 +3,14 @@
 from replacefunc import replaceFunc
 def newFunc(arg):  # 置換後の新しい関数。
     print("置換後の関数で{}を出力。".format(arg)) 
-@replaceFunc(print, newFunc)  # 引数付きデコレータ。グローバルスコープで使うと2回実行されてしまう。
+    
+def newFunc2(arg):
+    print("置換後の関数2で{}を出力。".format(arg))
+    
+@replaceFunc(print=newFunc, len=newFunc2)  # 引数付きデコレータ。グローバルスコープで使うと2回実行されてしまう。
 def targetFunc():  # この関数の中の関数を置換する。
     print("ターゲットの関数")
+    len("ターゲットの関数2")
+    
 targetFunc()
-
+targetFunc.__wrapped__()
--- RepalceFunc/src/replacefunc.py
+++ RepalceFunc/src/replacefunc.py
@@ -3,6 +3,15 @@
 from functools import wraps
 import inspect   
 def indentList(tab, first_line):
+    '''
+    Hack to deal with indented code
+    :param tab: String of one tab
+    :type tab: string
+    :param first_line: First line of code
+    :type first_line: string
+    :returns: an list of lines, Number of tabs at the beginning of the line
+    :rtype: list, int
+    '''
     ind = 'if 1:'
     ifs = list()
     first_line = first_line.expandtabs(4)  # 関数定義の先頭行をタブを4つのスペースに置換して取得。
@@ -10,23 +19,31 @@ def indentList(tab, first_line):
     for i in range(t):
         ifs.append(tab * i + ind)    
     return ifs, t
-def replaceFunc(oldfunc, newfunc):  # func内のoldfuncをnewfuncに置換する。
+def replaceFunc(**kwargs):  # func内のoldfuncをnewfuncに置換する。引数はキー:oldfunc名、値:newfunc関数オブジェクト、の辞書。newfuncはコードの文字列でも可。 @DontTrace
+    '''
+    Replace functions in the function to decorate
+    '''
     def decorate(func):
         tab = "    "  # タブの文字列をスペース4個とする。
-        newfunc_src = newfunc if isinstance(newfunc, str) else inspect.getsource(newfunc)
-        newfunc_srclines = newfunc_src.splitlines()
-        newfunclines, _ = indentList(tab, newfunc_srclines[0])
-        newfunclines.extend(newfunc_srclines)
         srclines = inspect.getsource(func).splitlines()  # funcのソースを1行ずつ要素にしたリストを取得。
         for n, line in enumerate(srclines):  # デコレータ式の行番号を取得。
             if '@replaceFunc' in line:
                 break        
         else:  # デコレータ式がないとき
             n = -1
-        funclines, t = indentList(tab, srclines[n+1])
-        srclines.insert(n + 2, tab * (t + 1) + "{} = {}".format(oldfunc.__name__, newfunc.__name__))  # func内に oldfunc = newfunc を挿入。
+        funclines, t = indentList(tab, srclines[n+1])  # インデントへの対応。tはタブの数。       
+        newfuncs = list()  # 新しい関数のコードを入れるリスト。   
+        tabs = tab * (t + 1)
+        exprs = tabs + "#replace functions"  # 挿入する式の文字列。
+        for oldfunc, newfunc in kwargs.items():  # 引数の辞書について。
+            newfunc_srclines = inspect.getsource(newfunc).splitlines()  # newfuncのコードの行をリストにする。
+            newfunclines, _ = indentList(tab, newfunc_srclines[0])  # インデント対応コードを取得。
+            newfunclines.extend(newfunc_srclines)  # newfuncのコードのリストを完成。
+            newfuncs.extend(newfunclines)
+            exprs += "\n" + tabs + "{} = {}".format(oldfunc, newfunc.__name__)
+        srclines.insert(n + 2, exprs)  # func内に oldfunc = newfunc を挿入。
         funclines.extend(srclines[n+1:])
-        funclines.extend(newfunclines)
+        funclines.extend(newfuncs)
         src = '\n'.join(funclines)  # funcのソースを再作成。
         temp = dict()  # exec()の仮想モジュールの名前空間を受けとる辞書。
         exec(compile(src,'generated in replaceFunc','exec'), temp, temp)  # srcをコンパイルしてtempに取得。
