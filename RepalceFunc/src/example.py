8bbda5fce5f8be95450dbd24bd9bbfc2abac095e
pq <pq.blogspot@gmail.com>
2017-06-03 08:14:14 +0900

    HM

--- RepalceFunc/src/example.py
+++ RepalceFunc/src/example.py
@@ -1,91 +1,12 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
-# from replacefunc import replaceFunc
-#  
-# def newFunc(arg):  # new function
-#     print("{} printed by the new function.".format(arg)) 
-#      
-# # def newFunc2(arg):  # new function2
-# #     print("{} printed by the new function2.".format(arg))
-#      
-# @replaceFunc(("__main__.print", newFunc),)  # old function = new function
-# def targetFunc():  # Replace functions in this function
-#     print("arg in the taget function")
-# 
-#      
-# targetFunc()  # Call the decorated function
-# 
-# targetFunc.__wrapped__()  # Call the wrapped function
-
-# 
-# from io import StringIO
-# from unittest.mock import patch
-# 
-# def targetFunc():  # Replace functions in this function
-#     print("arg in the taget function")
-#     len("arg2 in the taget function")
-# 
-# with patch("sys.stdout", new=StringIO()) as fake_out:  # sys.stdoutをStringIOに置換する。
-#     targetFunc()
-# 
-# print("{} モックからの出力。".format(fake_out.getvalue()))
-
-
-# from unittest.mock import patch
-# def targetFunc():
-#     print("ターゲットの関数")
-# def newFunc(arg):
-# #     print("置換後の関数で{}を出力。".format(arg))
-#     lst.append("置換後の関数で{}を出力。".format(arg))
-# def replaceFunc():
-#     pass   
-# if __name__ == "__main__":
-#     lst = []
-# #     with patch('builtins.print', side_effect = newFunc) as mock_print:
-#     with patch('__main__.print', side_effect = newFunc) as mock_print:  
-# #     with patch('print', side_effect = newFunc) as mock_print:      
-#         targetFunc()
-# #         print("test")
-# #     targetFunc()
-#     print(lst)
-
-# from unittest.mock import patch
-# def targetFunc():
-#     print("ターゲットの関数")
-# def newFunc(arg):
-#     print("置換後の関数で{}を出力。".format(arg))
-# #     lst.append("置換後の関数で{}を出力。".format(arg))
-# def replaceFunc():
-# #     lst = []
-# #     with patch('builtins.print', side_effect = newFunc) as mock_print:
-#     with patch('targetFunc.print', side_effect = newFunc):   
-#         targetFunc()
-#  
-# #     print(lst)   
-# if __name__ == "__main__":
-# #     lst = []
-#      
-#     replaceFunc()
-
-
-from functools import wraps
-from unittest.mock import patch
-
+from replacefunc import replaceFunc
 import replacefunc
 
-def targetFunc():
-    print("ターゲットの関数")
-def replaceFunc(func):
-    @wraps(func)
-    def wrapper():
-        with patch('__main__.print') as print_mock: 
-            print_mock.side_effect = replacefunc.newFunc
-            func()
-    return wrapper
-if __name__ == "__main__":
-    targetFunc = replaceFunc(targetFunc)
-    targetFunc()
-   
+# @replaceFunc({"__main__.print": newFunc, "__main__.len": newFunc2})  # key: old function, value: new function
+@replaceFunc({"__main__.print": replacefunc.newFunc})
+def targetFunc():  # Replace functions in this function
+    print("arg in the taget function")
+    len("arg in the taget function2")
     
-
-    
+targetFunc()  # Call the decorated function
--- RepalceFunc/src/replacefunc.py
+++ RepalceFunc/src/replacefunc.py
@@ -1,66 +1,28 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
+def newFunc(arg):  # new function
+    print("{} printed by the new function.".format(arg)) 
+    
+def newFunc2(arg):  # new function2
+    print("{} printed by the new function2.".format(arg))
 
-# import inspect   
-
-
-
-
-def newFunc(arg):
-    print("置換後の関数で{}を出力。".format(arg))
-   
-
-# def indentList(tab, first_line):
-#     '''
-#     Hack to deal with indented code
-#     :param tab: String of one tab
-#     :type tab: string
-#     :param first_line: First line of code
-#     :type first_line: string
-#     :returns: an list of lines, Number of tabs at the beginning of the line
-#     :rtype: list, int
-#     '''
-#     ind = 'if 1:'
-#     ifs = list()
-#     first_line = first_line.expandtabs(4)  # 関数定義の先頭行をタブを4つのスペースに置換して取得。
-#     t = first_line.replace(first_line.lstrip(tab), "").count(tab)  # 行頭のtabの数を取得。     
-#     for i in range(t):
-#         ifs.append(tab * i + ind)    
-#     return ifs, t
-# def replaceFunc(**kwargs):  # func内のoldfuncをnewfuncに置換する。引数はキー:oldfunc名、値:newfunc関数オブジェクト、の辞書。newfuncはコードの文字列でも可。 @DontTrace
-#     '''
-#     Replace functions in the function to decorate
-#     '''
-#     def decorate(func):
-#         tab = "    "  # タブの文字列をスペース4個とする。
-#         srclines = inspect.getsource(func).splitlines()  # funcのソースを1行ずつ要素にしたリストを取得。
-#         for n, line in enumerate(srclines):  # デコレータ式の行番号を取得。
-#             if '@replaceFunc' in line:
-#                 break        
-#         else:  # デコレータ式がないとき
-#             n = -1
-#         funclines, t = indentList(tab, srclines[n+1])  # インデントへの対応。tはタブの数。       
-#         tabs = tab * (t + 1)
-#         exprs = ""
-#         glb = dict()  # exec()の仮想モジュールのグローバル名前空間へ渡す辞書。
-#         for key, val in kwargs.items():  # 引数の辞書について。
-#             if isinstance(val, str):  # 値に文字列が渡された時
-#                 for m, line in enumerate(srclines[n+2:]):  # 関数の2行目から各行について
-#                     if key in line:  # keyをvalに置換する。
-#                         srclines[n+2+m] = line.replace(key, val)
-#             else:  # 値が文字列でないときは関数オブジェクトと考える。
-#                 exprs += "\n" + tabs + "{} = {}".format(key, val.__name__)
-#                 glb[val.__name__]  = val
-#         if exprs:
-#             exprs = tabs + "#replace functions" + exprs
-#             srclines.insert(n + 2, exprs)  # func内に oldfunc = newfunc を挿入。
-#         funclines.extend(srclines[n+1:])
-#         src = '\n'.join(funclines)  # funcのソースを再作成。
-#         loc = dict()  # exec()の仮想モジュールのローカル名前空間へ渡す辞書。
-#         exec(compile(src,'generated in replaceFunc','exec'), glb, loc)  # srcをコンパイルしてtempに取得。glbとlocは使われた後新しく書き換えられる。
-#         @wraps(func)
-#         def wrapper(*args, **kwargs):
-#             return loc[func.__name__](*args, **kwargs)  # 作り替えたfuncを返す。
-#         return wrapper
-#     return decorate
+from functools import wraps
+from unittest.mock import patch
+def replaceFunc(dic):  # func内のoldfuncをnewfuncに置換する。引数はキー:oldfunc名、値:newfunc関数オブジェクト、の辞書。
+    '''
+    Replace functions in the function to decorate
+    '''
+    def decorate(func):
+        for key, val in dic.items():
+            oldfunc = key
+            newfunc = val
+        
+        
+        
+        @wraps(func)
+        def wrapper(*args, **kwargs):    
+            with patch(oldfunc, side_effect=newfunc):  
+                func(*args, **kwargs)
+        return wrapper
+    return decorate
     
