f711fc07d969d32775273af7d767af8ff2a42c8b
pq <pq.blogspot@gmail.com>
2017-06-03 19:49:13 +0900

    stdout

--- RepalceFunc/src/example.py
+++ RepalceFunc/src/example.py
@@ -1,10 +1,37 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
-from redirectstdout import redirectStdout
+# from functools import wraps
+# from contextlib import redirect_stdout
+# from io import StringIO
+# def redirectStdout(func):  
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):  
+#         fake_out = StringIO()  
+#         with redirect_stdout(fake_out):  # 標準出力をリダイレクト。
+#             func(*args, **kwargs)
+#         print("リダイレクトされた出力{}".format(fake_out.getvalue()))
+#     return wrapper 
+# 
+# @redirectStdout
+# def targetFunc():  # Replace functions in this function
+#     print("arg in the taget function")
+# 
+# targetFunc()  # Call the decorated function
+
+
+from functools import wraps
+from unittest.mock import patch
+from io import StringIO
+def redirectStdout(func):  
+    @wraps(func)
+    def wrapper(*args, **kwargs):    
+        with patch("sys.stdout", new=StringIO()) as fake_out:  # 標準出力をリダイレクト。
+            func(*args, **kwargs)
+        print("リダイレクトされた出力{}".format(fake_out.getvalue()))
+    return wrapper 
 
 @redirectStdout
 def targetFunc():  # Replace functions in this function
     print("arg in the taget function")
 
-    
 targetFunc()  # Call the decorated function
--- RepalceFunc/src/rediretstdout.py
+++ RepalceFunc/src/rediretstdout.py
@@ -1,28 +1,15 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
-def newFunc(arg):  # new function
-    print("{} printed by the new function.".format(arg)) 
-    
-def newFunc2(arg):  # new function2
-    print("{} printed by the new function2.".format(arg))
-
 from functools import wraps
 from unittest.mock import patch
-from contextlib import ExitStack
-def replaceFunc(dic):  # func内のoldfuncをnewfuncに置換する。引数はキー:oldfunc名、値:newfunc関数オブジェクト、の辞書。
-    '''
-    Replace functions in the function to decorate
-    '''
-    def decorate(func):
-        ctx_mgrs = []  # コンテクストマネジャーを入れるリスト。
-        for key, val in dic.items():  # key: oldfunc, val: newfunc
-            ctx_mgrs.append(patch(key, side_effect=val))  # コンテクストマネジャーをリストに取得。    
- 
-        @wraps(func)
-        def wrapper(*args, **kwargs):    
-            with ExitStack() as stack:
-                for mgr in ctx_mgrs:
-                    stack.enter_context(mgr)  # コンテクストマネジャーのリストを展開。
-                func(*args, **kwargs)
-        return wrapper
-    return decorate
+from io import StringIO
+
+def redirectStdout(func):  
+    @wraps(func)
+    def wrapper(*args, **kwargs):    
+        with patch("sys.stdout", new=StringIO()) as fake_out:
+            func(*args, **kwargs)
+        print("リダイレクトされた出力{}".format(fake_out.getvalue()))
+    return wrapper
+    
+
