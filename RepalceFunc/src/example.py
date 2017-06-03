8e488d546be4d4c75ee6818e54e1ce71b6ad35f3
pq <pq.blogspot@gmail.com>
2017-06-03 18:21:58 +0900

    HM

--- RepalceFunc/src/example.py
+++ RepalceFunc/src/example.py
@@ -1,11 +1,10 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
-from replacefunc import replaceFunc
-import replacefunc
+from redirectstdout import redirectStdout
 
-@replaceFunc({"__main__.print": replacefunc.newFunc, "__main__.len": replacefunc.newFunc2})  # key: old function, value: new function
+@redirectStdout
 def targetFunc():  # Replace functions in this function
     print("arg in the taget function")
-    len("arg in the taget function2")
+
     
 targetFunc()  # Call the decorated function
---
+++ RepalceFunc/src/rediretstdout.py
@@ -0,0 +1,28 @@
+#!/opt/libreoffice5.2/program/python
+# -*- coding: utf-8 -*-
+def newFunc(arg):  # new function
+    print("{} printed by the new function.".format(arg)) 
+    
+def newFunc2(arg):  # new function2
+    print("{} printed by the new function2.".format(arg))
+
+from functools import wraps
+from unittest.mock import patch
+from contextlib import ExitStack
+def replaceFunc(dic):  # func内のoldfuncをnewfuncに置換する。引数はキー:oldfunc名、値:newfunc関数オブジェクト、の辞書。
+    '''
+    Replace functions in the function to decorate
+    '''
+    def decorate(func):
+        ctx_mgrs = []  # コンテクストマネジャーを入れるリスト。
+        for key, val in dic.items():  # key: oldfunc, val: newfunc
+            ctx_mgrs.append(patch(key, side_effect=val))  # コンテクストマネジャーをリストに取得。    
+ 
+        @wraps(func)
+        def wrapper(*args, **kwargs):    
+            with ExitStack() as stack:
+                for mgr in ctx_mgrs:
+                    stack.enter_context(mgr)  # コンテクストマネジャーのリストを展開。
+                func(*args, **kwargs)
+        return wrapper
+    return decorate
