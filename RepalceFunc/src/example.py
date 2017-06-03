081a3610f82a8591656d0eee5a6ee528edb56222
pq <pq.blogspot@gmail.com>
2017-06-03 16:49:58 +0900

    using patch

--- RepalceFunc/src/example.py
+++ RepalceFunc/src/example.py
@@ -1,27 +1,11 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
 from replacefunc import replaceFunc
+import replacefunc
 
-def newFunc(arg):  # new function
-    print("{} printed by the new function.".format(arg)) 
-    
-def newFunc2(arg):  # new function2
-    print("{} printed by the new function2.".format(arg))
-    
-@replaceFunc(print=newFunc, len=newFunc2, arg="arg2")  # old function = new function
+@replaceFunc({"__main__.print": replacefunc.newFunc, "__main__.len": replacefunc.newFunc2})  # key: old function, value: new function
 def targetFunc():  # Replace functions in this function
     print("arg in the taget function")
     len("arg in the taget function2")
     
 targetFunc()  # Call the decorated function
-
-def targetFunc2():  # Replace functions in this function
-    print("arg in the taget function")
-    len("arg in the taget function2")
-
-# If you want to display the edited code, add keyword argument.
-targetFunc2 = replaceFunc(print=newFunc)(targetFunc2, debug=True)
-targetFunc2()
-
-
-
